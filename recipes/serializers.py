2abc59843d7b48a58388bfbf413b77f3f676baaf
CozyPlantlady <laurajohanna.taipale@gmail.com>
2022-12-21 20:23:29 +0000

    Add RecipeList view

--- recipes/serializers.py
+++ recipes/serializers.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-from .recipes import Recipe
+from .models import Recipe
 
 
 class RecipeSerializer(serializers.ModelSerializer):
--- recipes/views.py
+++ recipes/views.py
@@ -1,3 +1,26 @@
-from django.shortcuts import render
+from rest_framework import status, permissions
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from .models import Recipe
+from .serializers import RecipeSerializer
 
-# Create your views here.
+
+class RecipeList(APIView):
+    serializer_class = RecipeSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+
+    def get(self, request):
+        recipe = Recipe.objects.all()
+        serializer = RecipeSerializer(
+            recipe, many=True, context={'request': request}
+            )
+        return Response(serializer.data)
+
+    def post(self, request):
+        serializer = RecipeSerializer(
+            data=request.data, context={'request': request}
+        )
+        if serializer.is_valid():
+            serializer.save(owner=request.user)
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
