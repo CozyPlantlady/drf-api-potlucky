cc32b8165d94721b452f039c2c7c66e7698ab0fb
CozyPlantlady <laurajohanna.taipale@gmail.com>
2022-12-21 21:00:55 +0000

    Add view to delete Recipe

--- recipes/views.py
+++ recipes/views.py
@@ -53,3 +53,10 @@ class RecipeDetail(APIView):
             serializer.save()
             return Response(serializer.data)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, pk):
+        recipe = self.get_object(pk)
+        recipe.delete()
+        return Response(
+            status=status.HTTP_204_NO_CONTENT
+        )
