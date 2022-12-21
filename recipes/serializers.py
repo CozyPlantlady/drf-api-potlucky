ac7452f91be36cf8030c178feb70baf0766eb61a
CozyPlantlady <laurajohanna.taipale@gmail.com>
2022-12-21 20:34:07 +0000

    Add Recipe image size validators

--- recipes/serializers.py
+++ recipes/serializers.py
@@ -8,6 +8,21 @@ class RecipeSerializer(serializers.ModelSerializer):
     profile_id = serializers.ReadOnlyField(source='owner.profile.id')
     profile_image = serializers.ReadOnlyField(source='owner.profile.image.url')
 
+    def validate_image(self, value):
+        if value-size > 1024 * 1024 * 2:
+            raise serializers.ValidationError(
+                'Image size too large. Keep it under 2MB'
+            )
+        if value.image.width > 4096:
+            raise serializers.ValidationError(
+                'Image width over 4096px, choose a smaller picture'
+            )
+        if value.image.height > 4096:
+            raise serializers.ValidationError(
+                'Image height over 4096px, choose a smaller picture'
+            )
+        return value
+
     def get_is_owner(self, obj):
         request = self.context['request']
         return request.user == obj.owner
