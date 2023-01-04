885716262b031b7e535592230bcda05c7ba68ce4
CozyPlantlady <laurajohanna.taipale@gmail.com>
2023-01-04 19:26:26 +0000

    Fix for cors origins problem

--- potlucky_drf_api/settings.py
+++ potlucky_drf_api/settings.py
@@ -70,6 +70,19 @@ ALLOWED_HOSTS = [
     'localhost',
 ]
 
+if 'CLIENT_ORIGIN' in os.environ:
+    CORS_ALLOWED_ORIGINS = [
+        os.environ.get('CLIENT_ORIGIN')
+    ]
+
+if 'CLIENT_ORIGIN_DEV' in os.environ:
+    extracted_url = re.match(
+        r'^.+-', os.environ.get('CLIENT_ORIGIN_DEV', ''), re.IGNORECASE
+    ).group(0)
+    CORS_ALLOWED_ORIGIN_REGEXES = [
+        rf"{extracted_url}(eu|us)\d+\w\.gitpod\.io$",
+    ]
+
 
 # Application definition
 
