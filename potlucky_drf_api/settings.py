c2d4e5b6bec8219260ff24c1f713636e574b5ece
CozyPlantlady <laurajohanna.taipale@gmail.com>
2023-01-05 17:12:50 +0000

    BUGFIX: add token authentication

--- potlucky_drf_api/settings.py
+++ potlucky_drf_api/settings.py
@@ -29,6 +29,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [(
+        'rest_framework.authentication.TokenAuthentication',
         'rest_framework.authentication.SessionAuthentication'
         if 'DEV' in os.environ
         else 'dj_rest_auth.jwt_auth.JWTCookieAuthentication'
