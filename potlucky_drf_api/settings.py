23da2c9d23c41f74162559caa5e4b235d5c0eeb2
CozyPlantlady <laurajohanna.taipale@gmail.com>
2023-01-06 18:15:04 +0000

    Redo for settings code that was changed during tutoring

--- potlucky_drf_api/settings.py
+++ potlucky_drf_api/settings.py
@@ -152,12 +152,17 @@ WSGI_APPLICATION = 'potlucky_drf_api.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-   'default': {
-       'ENGINE': 'django.db.backends.sqlite3',
-       'NAME': BASE_DIR / 'db.sqlite3',
-   }
-}
+if 'DEV' in os.environ:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
+else:
+    DATABASES = {
+        'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
+    }
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
